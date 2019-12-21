ad0241a3c4544b97dca7d69717f93e0b95ce9b82
ASchwad <a.schoenenwald@gmx.de>
2019-12-21 13:17:00 +0100

    Add allow-java-serialization

--- src/main/scala/BankingApp.scala
+++ src/main/scala/BankingApp.scala
@@ -4,11 +4,14 @@ import akka.persistence._
 
 import scala.concurrent.duration._
 
-case class Deposit(amount : Int)
-case class Withdraw(amount : Int)
+trait Message
+trait RecoveryEvent
+
+case class Deposit(amount : Int) extends Message
+case class Withdraw(amount : Int) extends Message
+
 case class DepositedEvent(amount : Int) extends RecoveryEvent
 case class WithdrawEvent(amount: Int) extends RecoveryEvent
-class RecoveryEvent()
 
 class TransactionService extends PersistentActor{
 
@@ -27,15 +30,18 @@ class TransactionService extends PersistentActor{
   }
 
   override def receiveCommand: Receive =  {
-    case Deposit(amount) => persist(DepositedEvent(amount))(updateBalance)
+    case Deposit(amount) => {
+      println("Test")
+      persist(DepositedEvent(amount))(updateBalance)
+    }
     case Withdraw(amount: Int) => {
       println(s"Withdraw $amount from Bank account")
     }
   }
 
   override def receiveRecover: Receive = {
-    case message @DepositedEvent(amount) => updateBalance(message)
-    case message @WithdrawEvent(amount) => updateBalance(message)
+    case message @ DepositedEvent(amount) => updateBalance(message)
+    case message @ WithdrawEvent(amount) => updateBalance(message)
   }
 }
 
@@ -46,6 +52,5 @@ object BankingApp extends App{
   val transactionService = system.actorOf(Props[TransactionService], "transaction-service");
 
   transactionService ! Deposit(123)
-  println("Send deposit message")
   //system.terminate()
 }
